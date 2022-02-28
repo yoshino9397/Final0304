0223afd8c22fe3eab979302170f0b46968502485
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-02-28 15:14:57 -0800

    theme

--- src/SetupForm.js
+++ src/SetupForm.js
@@ -7,7 +7,7 @@ const SetupForm = () => {
     <main>
       <section className="quiz quiz-small">
         <form className="setup-form">
-          <h2>setup quiz</h2>
+          <h2>Yoshino's Quiz</h2>
           {/* amount */}
           <div className="form-control">
             <label htmlFor="amount">number of questions</label>

d0b88b857702718b5e687a4dd9d1ba3989c8c8dd
k-e-r <damajonboua@gmail.com>
2022-03-02 21:58:55 -0800

    update build

---
+++ build/precache-manifest.afe6d5bc03e4f3c1478934de4a5f03e2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "272a59c041f6204a7e02a9fb0201f20c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2126afcc94b4272539ca",
+    "url": "/static/css/main.986a965b.chunk.css"
+  },
+  {
+    "revision": "fff792489310330c7657",
+    "url": "/static/js/2.090875c1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.090875c1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2126afcc94b4272539ca",
+    "url": "/static/js/main.9e9a1fa6.chunk.js"
+  },
+  {
+    "revision": "7402b3a51c6a0325ccf5",
+    "url": "/static/js/runtime-main.297f6f71.js"
+  }
+]);
